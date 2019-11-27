9739bbf17a0557076a63d8b70ad2958c230ac965
pedroggbcampos <pedrocasgcampos@live.com.pt>
2019-11-27 12:22:44 +0100

    changes... cha cha cha changesss

--- mst.py
+++ mst.py
@@ -1,36 +1,74 @@
 import sys
 import random
+import math
+import collections
+
 
 DEBUG = True # False when you submit to kattis
 
 N = 0
-MAX_WEIGHT = 0
-EPS = 0.0
+W = 0
+E = 0.0
 
 def approx_mst_weight():
+    global N, E, W
     estimators_sum = 0
-    for i in range(1, W):
+    for i in range(1, W - 1):
         estimators_sum += approx_connected_comps(i)
-    return n - max_weight + estimators_sum
+    return N - W + estimators_sum
 
 def approx_connected_comps(subgraph_weight):
-    global n
-    s = 5 # choose s                                            ???
+    global N, E, W
+    s = math.ceil(10/(E^2)) # choose s
     bi_sum = 0
     for i in range(1, s):
-        x = 5 # choose X according to Pr[X ≥ k] = 1/k           ???
-        bi_sum += bfs(x)
+        rand = 0.0
+        while rand == 0.0:
+            rand = random.uniform(1, 0)
+        x = 1/rand  # choose X according to Pr[X ≥ k] = 1/k : X = 1/rand(0, 1) behaves like this : for example, P(1/rand(0,1) >= 2 ) = 1/2
+
+        node_in_comp = False
+        while not node_in_comp:
+            node = random.randint(0, N-1)
+            neighbors = getNeighbors(node)
+            for neighbor in neighbors:
+                if neighbor[1] < subgraph_weight:
+                    node_in_comp = True
+                    break
+
+        bi_sum += bfs(node, neighbors, subgraph_weight, math.ceil(x))
     return (n/s)*bi_sum
 
-def bfs(max_nodes_visited):
-    return 0
+def bfs(node, neighbors, subgraph_weight, max_nodes_to_visit):
+    global N, E, W
+
+    root_node = node
+    visited = set()
+    queue = collections.deque([node])
+    visited.add(node)
+    n_visited = 0
+    while queue:
+        node = queue.popleft()
+        if node != root_node:
+            neighbors = getNeighbors(node)
+        for neighbor in neighbors:
+            if neighbor not in visited and neighbor[1] <= subgraph_weight:
+                visited.add(neighbor)
+                queue.append(neighbor)
+        n_visited += 1
+        if n_visited == max_nodes_to_visit:
+            break
+    if queue.empty():
+        return 1
+    else:
+        return 0
 
 if __name__ == '__main__':
-    global N, EPS, MAX_WEIGHT
+    global N, E, W
     if DEBUG:
-        N = 21000000 # the number of nodes
-        EPS = 0.1 # desired accuracy
-        MAX_WEIGHT = 3 # largest weight in our graph
+        N = 6 # the number of nodes
+        E = 0.1 # desired accuracy
+        W = 2 # largest weight in our graph
         def getNeighbors(node):
         	leftNeighbor = (node-1) % N
         	rightNeighbor = (node+1) % N
@@ -38,8 +76,8 @@ if __name__ == '__main__':
         	return [( leftNeighbor, weight), ( rightNeighbor, weight)]
     else:
         N = int(sys.stdin.readline()) # read number of nodes from the input
-    	EPS = float(sys.stdin.readline()) - 1 # we read the desired approximation
-    	MAX_WEIGHT = int(sys.stdin.readline()) # read the largest weight of the graph
+    	E = float(sys.stdin.readline()) - 1 # we read the desired approximation
+    	W = int(sys.stdin.readline()) # read the largest weight of the graph
         def getNeighbors(node):
         	# ask kattis for the next node
         	print(node)
